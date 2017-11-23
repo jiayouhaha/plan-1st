ce1d98093c85fbd093fc95ec93fbbd8a5a8e4aa1
mei.zhong <mei.zhong@cootek.cn>
2017-11-23 09:53:10 +0800

    plan

--- plan/js/index1.js
+++ plan/js/index1.js
@@ -1,9 +1,9 @@
 $(function(){
-	
 	var page=1;
 	var pagesize=10;
-	var loadDataStatus=0;
+	var loadDataStatus=0;//0:不在加载中;1:加载中;2:所有数据加载完毕
 	var totalData=[];
+	//刚进入页面时调用getData,将page、pagesize两个变量传过去
 	getData(page,pagesize)
 	
 	$(window).scroll(function(){
@@ -13,8 +13,8 @@ $(function(){
 		var scrollHeight=$(document).height();
 		//浏览器窗口高度
 		var windowHeight=$(this).height();
+
 		if(scrollTop+windowHeight==scrollHeight){
-			
 			if(loadDataStatus==2){
 				return;
 			}
@@ -32,6 +32,7 @@ $(function(){
 			dataType:'json',
 			data:null,
 			success:function(data){
+				//data.data.length==0表示所有数据加载完毕
 				if(data.data.length==0){
 					loadDataStatus=2;
 
